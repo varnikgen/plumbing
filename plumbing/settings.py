bf750af56b830198255247ea818c1d42ee153ac3
Nikolay Varfolomeev <varnikgen@yandex.ru>
2020-08-04 16:38:01 +1000

    создано django-приложение корзины и основной класс корзины

--- plumbing/settings.py
+++ plumbing/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'shop',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,5 @@ STATICFILES_DIRS = [STATIC_DIR]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CART_SESSION_ID = 'cart'
