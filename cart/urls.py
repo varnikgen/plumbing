bd05bf071772348d98df0f0cc54adc974e811af8
Nikolay Varfolomeev <varnikgen@yandex.ru>
2020-08-11 16:56:07 +1000

    новый контекстный процессор и замена метода cart_add на класс CartAddView

--- cart/urls.py
+++ cart/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'cart'
 urlpatterns = [
     path('', views.cart_detail, name='cart_detail'),
-    path('add/<int:pk>/', views.cart_add, name='cart_add'),
+    path('add/<int:pk>/', views.CartAddView.as_view(), name='cart_add'),
     path('remove/<int:pk>/', views.cart_remove, name='cart_remove'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,21 +1,34 @@
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST
+from django.views.generic.base import View
 
 from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
 
 
-@require_POST
-def cart_add(request, pk):
+# @require_POST
+# def cart_add(request, pk):
+#     """Добавление товара в корзину"""
+#     cart = Cart(request)
+#     product = Product.objects.get(id=pk)
+#     form = CartAddProductForm(request.POST)
+#     if form.is_valid():
+#         cd = form.cleaned_data
+#         cart.add(product=product, quantity=cd['quantity'], update_quantity=cd['update'])
+#     return redirect('cart:cart_detail')
+
+
+class CartAddView(View):
     """Добавление товара в корзину"""
-    cart = Cart(request)
-    product = Product.objects.get(id=pk)
-    form = CartAddProductForm(request.POST)
-    if form.is_valid():
-        cd = form.cleaned_data
-        cart.add(product=product, quantity=cd['quantity'], update_quantity=cd['update'])
-    return redirect('cart:cart_detail')
+    def post(self, request, pk):
+        cart = Cart(request)
+        product = Product.objects.get(id=pk)
+        form = CartAddProductForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            cart.add(product=product, quantity=cd['quantity'], update_quantity=cd['update'])
+        return redirect('cart:cart_detail')
 
 
 def cart_remove(request, pk):
